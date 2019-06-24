8391bc50724923ce3f436bc5d8720967a8d709c6
gnguedes <goncalo.alt@gmail.com>
2019-06-24 13:08:23 +0100

    comentarios

--- js/models/comments.js
+++ js/models/comments.js
@@ -1,3 +1,8 @@
+/**classe de comentarios que guarda o nome do 
+ * utilizador que fez o comentario, o país onde o fez,
+ * o comentario em si e a data
+ */
+
 export default class Comment{
     constructor(loggedUser, country, comment, date){
         this.loggedUser = loggedUser
